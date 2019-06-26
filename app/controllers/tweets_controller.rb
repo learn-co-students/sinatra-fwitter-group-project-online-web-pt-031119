a416d8b65836f74dc1ea77c7cd1391790e13e583
iamtash <natashachernis@gmail.com>
2019-06-26 01:45:43 -0500

    Done.

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,4 +1,70 @@
 class TweetsController < ApplicationController
 
+    get '/tweets' do
+        if @current_user = current_user
+            @tweets = Tweet.all
+            erb :'/tweets/index'
+        else 
+            redirect '/login'
+        end
+    end
+
+    get '/tweets/new' do 
+        if logged_in?
+            erb :'/tweets/new'
+        else  
+            redirect '/login'
+        end
+    end
+
+    post '/tweets' do 
+        if params[:content] != ""
+            tweet = Tweet.create(content: params[:content])
+            tweet.user = current_user
+            tweet.save
+        else  
+            redirect '/tweets/new'
+        end
+    end
+
+    delete '/tweets/:id' do 
+        if logged_in?
+            tweet = Tweet.find_by(id: params[:id])
+            if tweet.user == current_user 
+                tweet.destroy 
+            else  
+                redirect '/tweets'
+            end
+        else
+            redirect '/login'
+        end
+    end
+
+    get '/tweets/:id/edit' do
+        if logged_in? 
+            @tweet = Tweet.find_by(id: params[:id])
+            erb :'/tweets/edit'
+        else  
+            redirect '/login'
+        end
+    end
+    
+    patch '/tweets/:id' do 
+        tweet = Tweet.find_by(id: params[:id])
+        if params[:content] != ""
+            tweet.update(content: params[:content]) 
+        else  
+            redirect "/tweets/#{tweet.id}/edit"
+        end
+    end
+
+    get '/tweets/:id' do 
+        if logged_in?
+            @tweet = Tweet.find_by(id: params[:id])
+            erb :'/tweets/show'
+        else  
+            redirect '/login' 
+        end
+    end
 
 end
