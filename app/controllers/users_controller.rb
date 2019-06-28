0fbfc407c37b975dac13c39003db52a047ad0242
MKanko <mkanko66@gmail.com>
2019-06-27 23:17:24 -0700

    Done.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,23 @@
 class UsersController < ApplicationController
 
+    get '/signup' do
+        if !logged_in? 
+            erb :'users/signup'
+        else
+            redirect to '/tweets'
+        end 
+    end 
+
+    post '/signup' do
+        if !params[:username].empty? && !params[:email].empty? && !params[:password].empty?  
+            @user = User.new(params)
+            @user.save
+            session[:user_id] = @user.id
+            redirect to '/tweets'
+        else
+            redirect to '/signup'
+        end          
+    end 
+
 
 end
