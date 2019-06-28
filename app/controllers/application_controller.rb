5eb56657782d9bdc99bd3b0c54edd322e8895620
Alex Merecka <merecka@gmail.com>
2019-06-28 02:34:49 -0500

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,59 @@ class ApplicationController < Sinatra::Base
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
+    enable :sessions
+   		set :session_secret, "secret"
+	end
   end
 
+  get '/' do
+  	erb :index
+  end
+
+  get '/signup' do
+	erb :'users/signup'
+  end
+
+  post '/signup' do
+	if params[:username] != "" && params[:email] != "" && params[:password] != ""
+		@user = User.create(username: params[:username], email: params[:email], password: params[:password])
+		redirect '/tweets'
+	else
+		redirect '/signup'
+	end	
+  end	
+
+  get '/login' do
+  	if session[:id] = ""
+  		erb :login
+  	else
+  		redirect '/tweets'  #placeholder until individual show page is created
+  	end
+  end
+
+  post '/login' do
+  	if @user = User.find_by(email: params[:email], password: params[:password])
+  		session[:id] = @user.id
+  		redirect 'login/:id'  #placeholder until individual show page is created
+  	else
+  		redirect '/login'
+  	end
+  end
+
+  get '/logout' do
+  	if session[:id] != ""
+
+  	else
+  		redirect '/'
+  	end
+
+  	redirect '/login'
+  end
+
+  get '/tweets' do
+  	if session[:id] != ""
+  		erb :index
+  	else
+  		redirect '/login'
+  	end
 end
