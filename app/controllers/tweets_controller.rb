83c897ef8092587eab4b9f046e72de72b3cdbc3e
MKanko <mkanko66@gmail.com>
2019-06-29 17:26:35 -0700

    completed sinatra-fwitter-project

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -27,9 +27,47 @@ class TweetsController < ApplicationController
         redirect to '/tweets/new'  
     end
     
-    get '/tweets/:id' do 
-        @tweet = Tweet.find_by_id(params[:id]) 
-        erb :'/tweets/show' 
-    end 
+    get '/tweets/:id' do
+        if logged_in? 
+            @tweet = Tweet.find_by_id(params[:id]) 
+            erb :'/tweets/show'
+        else
+            redirect to '/login'
+        end 
+    end
+    
+    get '/tweets/:id/edit' do
+        if logged_in?
+            @tweet = Tweet.find_by_id(params[:id])
+            if @tweet && @tweet.user == current_user 
+                erb :'/tweets/edit'
+            end 
+        else
+            redirect to '/login'
+        end 
+    end
+    
+    patch '/tweets/:id' do
+        @tweet = Tweet.find_by_id(params[:id])
+        if params[:content] != ""
+            @tweet.update(content: params[:content])
+            redirect to "/tweets/#{@tweet.id}"
+        else
+            redirect to "/tweets/#{@tweet.id}/edit"
+        end 
+    end
+    
+    delete '/tweets/:id/delete' do
+        if logged_in?
+            @tweet = Tweet.find_by_id(params[:id])
+            if @tweet && @tweet.user == current_user
+                @tweet.delete
+            end                
+            redirect to '/tweets'
+        else 
+            redirect to '/login'
+        end
+    end  
+
 
 end
