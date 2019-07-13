f27dcf2c4d1b9a4461859a66b5e88714d2af17b1
Ryan Pethel <arpethel@gmail.com>
2019-07-13 22:35:37 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -18,7 +18,7 @@ class ApplicationController < Sinatra::Base
     end
 
     def current_user
-      @current_user ||= User.find(session[:user_id]) if session[:user_id]
+      User.find(session[:user_id]) if session[:user_id]
     end
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,7 +15,7 @@ class UsersController < ApplicationController
       redirect '/signup'
     else
       @user.save
-      session[:user_id] = @user_id
+      session[:user_id] = @user.id
       redirect '/tweets'
     end
   end
