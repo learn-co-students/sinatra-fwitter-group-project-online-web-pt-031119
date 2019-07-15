e0ea9950d882a7ea5c1c0ed37249d4aeb27fbff5
Kelly Sciandra <kellysciandradev@icloud.com>
2019-07-15 18:33:02 +0000

    Done.

--- db/migrate/01_create_users_table.rb
+++ db/migrate/01_create_users_table.rb
@@ -1,4 +1,4 @@
-class CreateUsersTable < ActiveRecord::Migration[5.2.2.1]
+class CreateUsersTable < ActiveRecord::Migration[5.2]
   def change
     create_table :users do |t|
       t.string :username
