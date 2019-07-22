0eaa3ae552276f548343368bb1aebe7fc0ab96ac
Dennis Chung <chungdennis6@gmail.com>
2019-07-22 14:58:24 +0000

    Done.

---
+++ app/helpers/helpers.rb
@@ -0,0 +1,12 @@
+class Helpers
+  
+  def self.current_user(session)
+    @user = User.find_by_id(session[:user.id])
+if @user.is_a?(user)  
+end
+end
+
+def self.is_logged_in?(session)
+  !!User.find_by_id(session[:user_id])
+end 
+end
